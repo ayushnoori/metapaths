9ac9d7b3f8de52b5e0b87fbd41559cfee44b8d47
an583 <anoori1@mgh.harvard.edu>
2021-08-05 11:30:21 -0700

    Updating node list, edge list, and reference list descriptions.

--- man-roxygen/edge-list.R
+++ man-roxygen/edge-list.R
@@ -1,2 +1,6 @@
-#' @param edge_list Edge list as a \code{data.table} which must contain the columns \code{Origin},
-#' \code{Destination}, \code{OriginType}, and \code{DestinationType}.
+#' @param edge_list Edge list as a \code{data.table} which must contain the following columns:
+#' \describe{
+#'   \item{\code{Origin}}{IDs of the origin nodes for each edge.}
+#'   \item{\code{Destination}}{IDs of the destination nodes for each edge.}
+#'   \item{\code{OriginType}}{Types of the origin node for each edge.}
+#'   \item{\code{DestinationType}}{Types of the destination node for each edge.}}
--- man-roxygen/node-list.R
+++ man-roxygen/node-list.R
@@ -1,4 +1,5 @@
-#' @param node_list Node list as a \code{data.table} which must contain the columns \code{Node}
-#' (corresponding to either \code{Origin} or \code{Destination} in the edge list), and \code{Node Type}
-#' (corresponding to either \code{OriginType} or \code{DestinationType} in the edge list). To disable type checking,
-#' set \code{node_list} to \code{"NoChecking"}.
+#' @param node_list Node list as a \code{data.table} which must contain the following columns:
+#' \describe{
+#'   \item{\code{Node}}{Node IDs (corresponding to either \code{Origin} or \code{Destination} in the edge list).}
+#'   \item{\code{NodeType}}{Node types (corresponding to either \code{OriginType} or
+#'   \code{DestinationType} in the edge list).}}
